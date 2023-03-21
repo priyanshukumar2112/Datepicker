d94157a962cdfe92352e0560b4aaed5a0d1c9583
priyanshukumar2112 <125110017+priyanshukumar2112@users.noreply.github.com>
2023-03-21 03:54:23 -0700

    Initial commit

---
+++ app/src/main/java/com/rare/datepicker/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.rare.datepicker
+
+import android.app.DatePickerDialog
+import android.content.pm.PackageManager
+import android.icu.util.Calendar
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.rare.datepicker.databinding.ActivityMainBinding
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import java.text.SimpleDateFormat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    var getImageContract = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        if(it){
+            Toast.makeText(this,"Permission granted", Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(this,"Permission not granted", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    var pickImage = registerForActivityResult(ActivityResultContracts.GetContent()){
+        //uri - universal resource identifier
+        System.out.println("in uri $it")
+        binding.ivIDproof.setImageURI(it)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnUploadImage.setOnClickListener {
+          if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+              pickImage.launch("image/*")
+          }else{
+              getImageContract.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+          }
+        }
+        binding.btnDatepicker.setOnClickListener {
+            var selectedCalenderDate = Calendar.getInstance()
+            var d = selectedCalenderDate.get(Calendar.DAY_OF_MONTH)
+            var m = selectedCalenderDate.get(Calendar.MONTH)
+            var y = selectedCalenderDate.get(Calendar.YEAR)
+
+            val datePickerDialog = DatePickerDialog(this, {_,year,month,day ->
+            selectedCalenderDate.set(year,month,day)
+        },y,m,d)
+            datePickerDialog.show()
+
+            val simpledateformat = SimpleDateFormat("dd/MM/YYYY")
+            val formatedDate = simpledateformat.format(selectedCalenderDate.time)
+            binding.tvDatepicker.setText(formatedDate)
+        }
+    }
+
+}
